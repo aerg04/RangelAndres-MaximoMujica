a00204b496af3286ff9ff573b70f203fab3cfdbc
maximomjp <122284163+maximomjp@users.noreply.github.com>
2023-07-02 10:09:54 -0400

    Add files via upload

---
+++ RangelAndres-MujicaMaximo_interfaces2/RangelAndres-MujicaMaximo/src/main/java/App/SearchClient.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package App;
+
+import primitivas.*;
+/**
+ *
+ * @author DELL
+ */
+public class SearchClient {
+
+    public SearchClient() {
+    }
+    public CurrentClient getInfoClient(String name, HashTable database ){
+        List list = database.search(name.replace(" ", "").toLowerCase());
+        if(list == null){
+            NodoList <CurrentClient> pAux;
+            pAux = list.getHead();
+            if(list.getSize() == 1){
+                return pAux.getValue();
+            }else{
+                while(pAux != null){
+                    if(name.equals((pAux.getValue().getFirst_name() + pAux.getValue().getFirst_name()).replace(" ", "").toLowerCase())){
+                        return pAux.getValue();
+                    }
+                    pAux = pAux.getpNext();
+                }
+            }
+        }
+//          else innecesario
+            return null;
+    }
+}
